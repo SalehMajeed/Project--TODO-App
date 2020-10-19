9d447bacfdc266507e06160381a5539891214a1f
= <saleh.majeed8@gmail.com>
2020-10-19 15:38:51 +0530

    Animation Phase

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ const todo_input = document.querySelector('.todo-input');
 const todo_button = document.querySelector('.todo-button');
 const todo_list = document.querySelector('.todo-list');
 
+// Event Listners
 todo_button.addEventListener('click', add_todo);
+todo_list.addEventListener('click', delete_check);
 
 function add_todo(event) {
     // Prevent form from submitting
@@ -34,5 +36,27 @@ function add_todo(event) {
     todo_list.appendChild(todo_div);
 
     //Clear Todo Input Value
-    todo_input.value = ''
+    todo_input.value = '';
+}
+
+function delete_check(event) {
+    const item = event.target;
+
+    //Delete Todo
+    if (item.classList[0] == 'trash-btn') {
+        const todo = item.parentElement;
+
+        //Animation
+        todo.classList.add('fall')
+        todo.addEventListener('transitioned',function(){
+            todo.remove()
+        })
+    }
+
+    //Check Mark
+    if (item.classList[0] == 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+        console.log('innn',todo)
+    }
 }
